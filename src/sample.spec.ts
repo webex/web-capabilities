24fabea5a50e875541714518a1af8d660cbd1924
Bryce Tham <brycetham@users.noreply.github.com>
2023-04-05 15:53:27 -0400

    feat: add GitHub actions and remove boilerplate samples (#1)
    * feat: add GitHub actions
    
    * fix: update .npmrc
    
    * fix: update yarn.lock
    
    * chore: remove boilerplate samples
    
    ---------
    
    Co-authored-by: Bryce Tham <btham@cisco.com>

--- src/sample.spec.ts
+++
@@ -1,8 +0,0 @@
-import { sampleMethod } from './sample';
-
-describe('sample Method', () => {
-  it('should return 4', () => {
-    expect.assertions(1);
-    expect(sampleMethod()).toBe(4);
-  });
-});
--- src/sample.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Sample Method.
- *
- * @returns The number 4.
- */
-export const sampleMethod = () => {
-  return 2 + 2;
-};
