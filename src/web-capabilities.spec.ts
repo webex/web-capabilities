7f6f0ba5950d01943e0e84017e3fcadb70b8fb99
Bryce Tham <btham@cisco.com>
2023-10-20 13:50:02 -0400

    feat: add API for virtual background

--- src/web-capabilities.spec.ts
+++ src/web-capabilities.spec.ts
@@ -13,38 +13,42 @@ const mockCpuInfo = (cpuInfo: CpuInfo) => {
 
 describe('WebCapabilities', () => {
   it('should get correct capabilities for a machine with unknown CPU information', () => {
-    expect.assertions(3);
+    expect.assertions(4);
 
     mockCpuInfo({});
 
     expect(WebCapabilities.isCapableOfBackgroundNoiseRemoval()).toBe(CapabilityState.UNKNOWN);
+    expect(WebCapabilities.isCapableOfVirtualBackground()).toBe(CapabilityState.UNKNOWN);
     expect(WebCapabilities.isCapableOfReceiving1080pVideo()).toBe(CapabilityState.UNKNOWN);
     expect(WebCapabilities.isCapableOfSending1080pVideo()).toBe(CapabilityState.UNKNOWN);
   });
   it('should get correct capabilities for a 1-core machine', () => {
-    expect.assertions(3);
+    expect.assertions(4);
 
     mockCpuInfo({ numLogicalCores: 1 });
 
     expect(WebCapabilities.isCapableOfBackgroundNoiseRemoval()).toBe(CapabilityState.NOT_CAPABLE);
+    expect(WebCapabilities.isCapableOfVirtualBackground()).toBe(CapabilityState.NOT_CAPABLE);
     expect(WebCapabilities.isCapableOfReceiving1080pVideo()).toBe(CapabilityState.NOT_CAPABLE);
     expect(WebCapabilities.isCapableOfSending1080pVideo()).toBe(CapabilityState.NOT_CAPABLE);
   });
   it('should get correct capabilities for a 2-core machine', () => {
-    expect.assertions(3);
+    expect.assertions(4);
 
     mockCpuInfo({ numLogicalCores: 2 });
 
     expect(WebCapabilities.isCapableOfBackgroundNoiseRemoval()).toBe(CapabilityState.CAPABLE);
+    expect(WebCapabilities.isCapableOfVirtualBackground()).toBe(CapabilityState.CAPABLE);
     expect(WebCapabilities.isCapableOfReceiving1080pVideo()).toBe(CapabilityState.CAPABLE);
     expect(WebCapabilities.isCapableOfSending1080pVideo()).toBe(CapabilityState.NOT_CAPABLE);
   });
   it('should get correct capabilities for a 8-core machine', () => {
-    expect.assertions(3);
+    expect.assertions(4);
 
     mockCpuInfo({ numLogicalCores: 8 });
 
     expect(WebCapabilities.isCapableOfBackgroundNoiseRemoval()).toBe(CapabilityState.CAPABLE);
+    expect(WebCapabilities.isCapableOfVirtualBackground()).toBe(CapabilityState.CAPABLE);
     expect(WebCapabilities.isCapableOfReceiving1080pVideo()).toBe(CapabilityState.CAPABLE);
     expect(WebCapabilities.isCapableOfSending1080pVideo()).toBe(CapabilityState.CAPABLE);
   });
