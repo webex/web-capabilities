3ecef579bd9ca2e2248fcb5ef7d4d9d4d34e2a49
evujici <evujici@cisco.com>
2024-05-22 14:20:51 -0400

    refactor: change name of tests

--- src/cpu-info.spec.ts
+++ src/cpu-info.spec.ts
@@ -1,7 +1,7 @@
 import { CpuInfo } from './cpu-info';
 
 describe('CpuInfo', () => {
-  it('should return the number of logical cores when it is available', () => {
+  it('should return the number of logical CPU cores when the information is available', () => {
     expect.assertions(1);
 
     jest.spyOn(Navigator.prototype, 'hardwareConcurrency', 'get').mockReturnValue(1);
@@ -9,7 +9,7 @@ describe('CpuInfo', () => {
     expect(CpuInfo.getNumLogicalCores()).toBe(1);
   });
 
-  it('should return undefined when the logical cores it is not available', () => {
+  it('should return undefined when the logical CPU cores information is not available', () => {
     expect.assertions(1);
 
     jest.spyOn(Navigator.prototype, 'hardwareConcurrency', 'get').mockImplementation();
